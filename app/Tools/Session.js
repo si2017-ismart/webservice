2e05d4e801eb6be3f6a425d8fbd8d74c56a001f4
luap <paul.kerguillec@gmail.com>
2017-01-11 19:44:44 +0100

    Tokens implemented

--- app/Tools/Session.js
+++ app/Tools/Session.js
@@ -1,42 +1,29 @@
-// Création d'un token d'aide à la personne 
-function Session()
-{
-	var date = new Date();
-	var myId = "";
-	var session;
+module.exports={
 
-	/**
-	 * Création de l'objet JSON de session
-	 * @param  {[type]} beacon_id   [description]
-	 * @param  {[type]} beacon_name [description]
-	 * @param  {[type]} position    [description]
-	 * @param  {[type]} name        [description]
-	 * @param  {[type]} sex         [description]
-	 * @return {[type]}             [description]
-	 */
-	this.create = function(beacon_id, beacon_name, position, name, sex)
+	// Création d'un token d'aide à la personne
+	function Session()
 	{
-		alea = Math.floor((Math.random() * 1000000) + 1)
-		this.myId = sha256(this.date+alea+beacon);
-		this.session = {
-			"id": this.myId,
-			"beacon": {
-				"id": 		beacon_id,
-				"nom": 		beacon_name,
-				"position": position
-			},
-			"user": {
-				"nom": 		name,
-				"sexe": 	sex,
-				"type": 	profil
-			},
-			"date": 		date,
-			"prise": 		false
-		};
-	}
+		var date = new Date();
+		var myId = "";
+		var session;
 
-	this.getSession = function()
-	{
-		return this.session;
+		/**
+		 * Création de l'objet JSON de session
+		 * @param  {[type]} beacon_id   [description]
+		 * @param  {[type]} beacon_name [description]
+		 * @param  {[type]} position    [description]
+		 * @param  {[type]} name        [description]
+		 * @param  {[type]} sex         [description]
+		 * @return {[type]}             [description]
+		 */
+		this.create = function(beacon_id, beacon_name, position, name, sex)
+		{
+
+		}
+
+		this.getSession = function()
+		{
+			return this.session;
+		}
 	}
-}
+};
